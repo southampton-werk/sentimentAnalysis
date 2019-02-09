1b0bb95e5c033e9eaf0fdbaf3c0237ea200791ce
jal2g15@soton.ac.uk <Jakeronie7>
2019-02-09 19:52:04 +0000

    server started

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -12,11 +12,11 @@ train_X = []
 size_train_x = int(0.8*len(content))
 
 while len(train_X) <= size_train_x:
-	index = randrange(len(content))
-	line = content.pop(index)
-	x_y = line.split("\t")
-	train_X.append([x.lower() for x in x_y[1].split(" ")])
-	train_Y.append(int(x_y[0]))
+    index = randrange(len(content))
+    line = content.pop(index)
+    x_y = line.split("\t")
+    train_X.append([x.lower() for x in x_y[1].split(" ")])
+    train_Y.append(int(x_y[0]))
 
 
 
@@ -26,40 +26,45 @@ stopwords =["i","be","by","at","up","this","was","some","if","have","been","will
 positive_count = {}
 negative_count = {}
 
+counter = 0
 for i in range(len(train_X)):
-	lines = train_X[i]
-	for words in lines:
-		if words in stopwords or len(words) == 1 or "\\" in words or words == ' ':
-			continue
-		if "." in words:
-			for j in range(len(words)):
-				if j == ".":
-					break
-				words = words[:j]
-		if "!" in words:
-			for j in range(len(words)):
-				if j == ".":
-					break
-				words = words[:j]
-		if words.isalpha() is False:
-			continue
-		if train_Y[i] == 1:
-			if words in positive_count:
-				positive_count[words]+=1
-			else:
-			 	positive_count[words]=1
-		else:
-			if words in negative_count:
-				negative_count[words]+=1
-			else:
-			 	negative_count[words]=1
+    counter = counter + 1
+    lines = train_X[i]
+    for words in lines:
+        if words in stopwords or len(words) == 1 or "\\" in words or words == ' ':
+            continue
+        if "." in words:
+            for j in range(len(words)):
+                if j == ".":
+                    break
+                words = words[:j]
+        if "!" in words:
+            for j in range(len(words)):
+                if j == ".":
+                    break
+                words = words[:j]
+        if words.isalpha() is False:
+            continue
+        if train_Y[i] == 1:
+            if words in positive_count:
+                positive_count[words]+=1
+            else:
+                 positive_count[words]=1
+        else:
+            if words in negative_count:
+                negative_count[words]+=1
+            else:
+                 negative_count[words]=1
 
 
+print(counter)
+print(len(positive_count))
+print(len(negative_count))
 
 
 #Sigmoid
 def sigmoid(input):
-	return 2/(1+np.exp(-input)) - 1
+    return 2/(1+np.exp(-input)) - 1
 
 #Sigmoid Gradient
 def sigmoid_grad(x):
@@ -69,28 +74,27 @@ def getSentiment(lines):
     lines = lines.split(" ")
     sentence_score = 0
     for words in lines:
-    	if words in stopwords or len(words) == 1 or "\\" in words or words == ' ':
-    		continue
-    	if "." in words:
-    		for j in range(len(words)):
-    			if j == ".":
-    				break
-    			words = words[:j]
-    	if "!" in words:
-    		for j in range(len(words)):
-    			if j == ".":
-    				break
-    			words = words[:j]
-    	if words.isalpha() is False:
-    		continue
-    	total = 0
-    	if words in positive_count:
-    		total+=positive_count[words]
-    	if words in negative_count:
-    		total+=negative_count[words]
-    	if words in positive_count:
-    		sentence_score += positive_count[words]/float(total)
-    	if words in negative_count:
-    		sentence_score -= negative_count[words]/float(total)
-
-print(sigmoid(sentence_score))
+        if words in stopwords or len(words) == 1 or "\\" in words or words == ' ':
+            continue
+        if "." in words:
+            for j in range(len(words)):
+                if j == ".":
+                    break
+                words = words[:j]
+        if "!" in words:
+            for j in range(len(words)):
+                if j == ".":
+                    break
+                words = words[:j]
+        if words.isalpha() is False:
+            continue
+        total = 0
+        if words in positive_count:
+            total+=positive_count[words]
+        if words in negative_count:
+            total+=negative_count[words]
+        if words in positive_count:
+            sentence_score += positive_count[words]/float(total)
+        if words in negative_count:
+            sentence_score -= negative_count[words]/float(total)
+    return (sigmoid(sentence_score))
