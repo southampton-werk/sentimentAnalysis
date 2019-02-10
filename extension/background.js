9357135fc03e5c5dc6fe813c89b0187afebcde87
U-SOTON\sj2g17 <sj2g17@JFB16-R2001-P10.soton.ac.uk>
2019-02-10 12:00:10 +0000

    idk

--- extension/background.js
+++ extension/background.js
@@ -5,32 +5,18 @@ $(document).ready(function(){
 		  console.log("Message analysis enabled");
 		});
 	});
-	// Takes a string, returns float given by semantic analysis of that string.
-	function evaluate(input){
-		console.log(input);
-		var out = 0.0;
-		if(false){
-			$.ajax({
-				type: 'GET',
-				crossDomain: true,
-				dataType: 'float',
-				url: "https://icebreakersentiment.appspot.com/sentiment/"+input,
-				success: function (data) {
-					out = data;
-				}
-			});
-		}
-		return (Math.random()*2)-1;
+	// Takes a string, returns float given by sentiment analysis of that string.
+	function evaluate(input, func){
+		$.get("https://icebreakersentiment.appspot.com/sentiment/"+input, func(data));
 	}
 
 	/* 
-		Respond to messages requesting semantic analysis with the result of passing their provided input to the evaluate function.
-		Semantic analysis request messages should have data of type {type: "input", data: <string>}.
+		Respond to messages requesting sentiment analysis with the result of passing their provided input to the evaluate function.
+		Sentiment analysis request messages should have data of type {type: "input", data: <string>}.
 	*/
 	chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 		if(message.type == "input"){
-			var sem = evaluate(message.data);
-			sendResponse({data: sem});
+			evaluate(message.data, sendResponse);
 		}
 	});
 	
@@ -42,26 +28,19 @@ $(document).ready(function(){
 	});
 	
 	chrome.contextMenus.onClicked.addListener(function(info, tab){
-		var semval = evaluate(info.selectionText)
-		var level = "";
-		if(semval<-0.75){
-			level = "SUPER negative";
-		}else if(semval<-0.5){
-			level = "Really negative";
-		}else if(semval<-0.25){
-			level = "Negative";
-		}else if(semval<0){
-			level = "Neutral";
-		}else if(semval<0.25){
-			level = "Neutral";
-		}else if(semval<0.5){
-			level = "Positive";
-		}else if(semval<0.75){
-			level = "Really positive";
-		}else{
-			level = "SUPER positive";
-		}
-		console.log(semval);
-		alert("Result: "+semval.toFixed(3)+"\n"+level);
+		$.get("https://icebreakersentiment.appspot.com/sentiment/"+info.selectionText, function(data){
+			var level = "";
+			if(data<-0.75)     { level = "SUPER negative";  }
+			else if(data<-0.5) { level = "Really negative"; }
+			else if(data<-0.25){ level = "Negative";        }
+			else if(data<0)    { level = "Neutral";         }
+			else if(data<0.25) { level = "Neutral";         }
+			else if(data<0.5)  { level = "Positive";        }
+			else if(data<0.75) { level = "Really positive"; }
+			else               { level = "SUPER positive";  }
+			
+			console.log(data);
+			alert("Result: "+data.substring(0, 5)+"\n"+level);
+		});
 	});
 });
